c1cd2a6c969b9b477dfa329583e40c2b06f1189b
MoFei <payer1233@gmail.com>
2020-04-04 00:40:11 +0300

    Updates

---
+++ precache-manifest.cae9c96dbfb109fe41835ba9c4b3cb6e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ad170d1abb877d1cda9129e725b4f6",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "a9f9794d1149814c9610",
+    "url": "/test/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "cf944303dd638cd32375",
+    "url": "/test/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "a9f9794d1149814c9610",
+    "url": "/test/static/js/2.97bd0e74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.97bd0e74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf944303dd638cd32375",
+    "url": "/test/static/js/main.4e7d11f5.chunk.js"
+  },
+  {
+    "revision": "ee0dd26b902cc09d5713",
+    "url": "/test/static/js/runtime-main.0faae597.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/test/static/media/persik.4e1ec840.png"
+  }
+]);
